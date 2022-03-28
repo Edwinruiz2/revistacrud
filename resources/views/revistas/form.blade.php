602dc7877f907c87b40494e37dda2f55e5cfd25c
Edenilson Villalobos <85265500+EdenilsonVillalobos@users.noreply.github.com>
2022-03-27 22:56:25 -0600

    Update form.blade.php

--- resources/views/revistas/form.blade.php
+++ resources/views/revistas/form.blade.php
@@ -13,22 +13,22 @@
   <div class="col-md-4 position-relative">
     
 
-<label for="validationTooltip01" class="form-label">Nombre:</label>
+<label for="validationTooltip01" class="fw-bold">Nombre:</label>
 <input type="text" name="name" class="form-control is-valid" id="validationTooltip01" value="{{isset($revista->name)?$revista->name:''}}" required>
-<label for="validationTooltip02" class="form-label">isbn:</label>
+<label for="validationTooltip02" class="fw-bold">isbn:</label>
 <input type="number" name="isbn"class="form-control is-valid" id="validationTooltip02"value="{{isset($revista->isbn)?$revista->isbn:''}}"required>
-<label for="validationServer03" class="form-label">Fecha:</label>
+<label for="validationServer03" class="fw-bold">Fecha:</label>
 <input type="date" name="date" class="form-control is-valid" id="validationServer03"value="{{isset($revista->date)?$revista->date:''}}" required>
-<label for="validationServer04" class="form-label">Tipo de revista:</label>
+<label for="validationServer04" class="fw-bold">Tipo de revista:</label>
 <input type="text" name="type" class="form-control is-valid" id="validationServer04"value="{{isset($revista->type)?$revista->type:''}}"required>
-<label for="validationServer05" class="form-label">Director de la revista:</label>
+<label for="validationServer05" class="fw-bold">Director de la revista:</label>
 <input type="text" name="director" class="form-control is-valid" id="validationServer05"value="{{isset($revista->director)?$revista->director:''}}"required>
 <div class="valid-tooltip">
       Looks good!
     </div>
 </div>
 <br>
-<br><button type="submit">Guardar</button>
+<br><button type="submit" class="btn btn-secondary">Guardar</button>
 </form>
 
 </body>
